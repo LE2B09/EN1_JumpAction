d36bad6d322c781859bae9f265d003d188286089
キクチケンタ <K023G0039@g.neec.ac.jp>
2024-06-03 12:36:39 +0900

    マウスの動き

--- Assets/ArrowDraw.cs
+++ Assets/ArrowDraw.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 using UnityEngine.UI;
 
 public class ArrowDraw : MonoBehaviour
 {
     [SerializeField]
     private Image arrowImage;
-    private Vector3 mousePosition;
+    private Vector3 clickPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,23 @@ public class ArrowDraw : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 dist = clickPosition - Input.mousePosition;
+            //ベクトルの長さを算出
+            float size = dist.magnitude;
+            //ベクトルから角度（弧度法）を算出
+            float angleRad = Mathf.Atan2(dist.y, dist.x);
+            //矢印の画像をクリックした場所に画像を移動
+            arrowImage.rectTransform.position = clickPosition;
+            //矢印の画像をベクトルから算出した角度を度数法に変換してZ回転
+            arrowImage.rectTransform.rotation = Quaternion.Euler(0, 0, angleRad * Mathf.Rad2Deg);
+            //矢印の画像の大きさをドラッグした距離に合わせる
+            arrowImage.rectTransform.sizeDelta = new Vector2(size, size);
+        }
     }
 }
