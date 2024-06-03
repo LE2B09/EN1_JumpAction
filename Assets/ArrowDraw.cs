f2d2e8900317b15ce286e43b0e0db8c10c4e4f87
キクチケンタ <K023G0039@g.neec.ac.jp>
2024-06-03 12:22:29 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/ArrowDraw.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowDraw : MonoBehaviour
+{
+    [SerializeField]
+    private Image arrowImage;
+    private Vector3 mousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
