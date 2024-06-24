a28702053e3ebe4f598ed6520e2a368d2445988c
キクチケンタ <K023G0039@g.neec.ac.jp>
2024-06-24 11:20:47 +0900

    EN1_JumpAction

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class ItemScript : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         animator.SetTrigger("Get");
+        audioSource.Play();
         Debug.Log("ÉAÉCÉeÉÄÇèEÇ¡ÇΩ");
     }
     private void DestroySelf()
